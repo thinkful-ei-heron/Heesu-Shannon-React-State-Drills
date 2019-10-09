158a3dad7b5766a1341220cbddecd3c77e34b5ef
Heesu Student <hskang135@gmail.com>
2019-10-09 12:18:24 -0400

    bomb

---
+++ src/Bomb.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+
+class Bomb extends React.Component {
+    state={
+        count: 0,
+    }
+
+    componentDidMount() {
+        this.interval = setInterval(() => {
+          this.setState({
+            count: this.state.count += 1
+          })
+        }, 1000)
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
+    }
+
+    display() {
+        if(this.state.count >= 8) {
+            return 'BOOM!!'
+        } else if (this.state.count % 2 === 0){
+            return 'tick'
+        } else {
+            return 'tock'
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                <p>{this.display}</p>
+            </div>
+        )
+    }
+
+}
+
+export default Bomb;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import HelloWorld from './HelloWorld';
+import Bomb from './Bomb';
 
-ReactDOM.render(<HelloWorld />, document.getElementById('root'));
+ReactDOM.render(<Bomb />, document.getElementById('root'));
